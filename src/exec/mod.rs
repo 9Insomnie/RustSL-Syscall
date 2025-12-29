d41a460a4d095651f14753ec7efe3c7c5ac57cce
Iwindy <12398041+iwindy0@user.noreply.gitee.com>
2025-12-29 13:45:34 +0800

    update

--- src/exec/mod.rs
+++ src/exec/mod.rs
@@ -1,12 +1,12 @@
 #[cfg(feature = "run_nt_create_thread_ex")]
-mod create_thread_syscall;
+mod create_thread;
 #[cfg(feature = "run_nt_create_thread_ex")]
-pub use crate::exec::create_thread_syscall::exec;
+pub use crate::exec::create_thread::exec;
 
 #[cfg(feature = "run_apc_syscall")]
-mod apc_syscall;
+mod apc;
 #[cfg(feature = "run_apc_syscall")]
-pub use crate::exec::apc_syscall::exec;
+pub use crate::exec::apc::exec;
 
 #[cfg(feature = "run_hook_bypass")]
 mod hook_bypass;
@@ -14,9 +14,9 @@ mod hook_bypass;
 pub use crate::exec::hook_bypass::exec;
 
 #[cfg(feature = "run_early_exception_inject")]
-mod early_exception_inject;
+mod early_exception;
 #[cfg(feature = "run_early_exception_inject")]
-pub use crate::exec::early_exception_inject::exec;
+pub use crate::exec::early_exception::exec;
 
 #[cfg(feature = "run_early_cascade")]
 mod early_cascade;
@@ -24,9 +24,9 @@ mod early_cascade;
 pub use crate::exec::early_cascade::exec;
 
 #[cfg(feature = "run_entry_point_injection")]
-mod entry_point_injection;
+mod entry_point;
 #[cfg(feature = "run_entry_point_injection")]
-pub use crate::exec::entry_point_injection::exec;
+pub use crate::exec::entry_point::exec;
 
 #[cfg(feature = "run_pool_party")]
 mod pool_party;
